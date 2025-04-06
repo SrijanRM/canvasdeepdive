4f2faa434abd557d0cdf839bb7f3621845f76e14
unknown <srijancodex@gmail.com>
2025-04-06 18:15:06 +0530

    rotate using angle

--- script.js
+++ script.js
@@ -40,21 +40,33 @@ class FlowFieldEffect {
     constructor(cxt, width, height) {
         this.#ctx = cxt;
         this.#height = height;
-        this.#ctx.strokeStyle = 'white';
-        this.#ctx.lineWidth = 5;
+        // this.#ctx.strokeStyle = 'white';
+        this.#ctx.lineWidth = 1;
         this.#width = width;
         // this.angle = 0;
         this.lastTime = 0;
         this.interval = 1000 / 60;
         this.timer = 0;
-        this.cellSize = 15
+        this.cellSize = 12;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
+    }
+    #createGradient() {
+        this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height)
+        this.gradient.addColorStop("0.1", "#ff5c33");
+        this.gradient.addColorStop("0.2", "#ff66b3");
+        this.gradient.addColorStop("0.4", "#ccccff");
+        this.gradient.addColorStop("0.6", "#b2ffff");
+        this.gradient.addColorStop("0.8", "#80ff80");
+        this.gradient.addColorStop("0.9", "#ffff33");
     }
     // private method 
-    #drawLine(x, y) {
+    #drawLine(angle, x, y) {
         const length = 30;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(x + 5, y + 5);
+        this.#ctx.lineTo(x + Math.cos(angle) * 20, y + Math.sin(angle) * 20);
         // this.#ctx.lineTo(mouse.x, mouse.y);
         this.#ctx.stroke();
     }
@@ -71,7 +83,8 @@ class FlowFieldEffect {
 
             for (let y = 0; y < this.#height; y += this.cellSize) {
                 for (let x = 0; x < this.#width; x += this.cellSize) {
-                    this.#drawLine(x, y);
+                    const angle = Math.cos(x * 0.05) + Math.sin(y * 0.05)
+                    this.#drawLine(angle, x, y);
                 }
             }
 
