6784e9f641e60c2bb0a028c12adeae8396c3e7b3
unknown <srijancodex@gmail.com>
2025-04-06 18:02:55 +0530

    grid in canvas using vector

--- script.js
+++ script.js
@@ -43,17 +43,19 @@ class FlowFieldEffect {
         this.#ctx.strokeStyle = 'white';
         this.#ctx.lineWidth = 5;
         this.#width = width;
-        this.angle = 0;
+        // this.angle = 0;
         this.lastTime = 0;
-        this.interval = 1000/60;
+        this.interval = 1000 / 60;
         this.timer = 0;
+        this.cellSize = 15
     }
     // private method 
-    #draw(x, y) {
+    #drawLine(x, y) {
         const length = 30;
         this.#ctx.beginPath();
         this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(mouse.x, mouse.y);
+        this.#ctx.lineTo(x + 5, y + 5);
+        // this.#ctx.lineTo(mouse.x, mouse.y);
         this.#ctx.stroke();
     }
 
@@ -64,9 +66,16 @@ class FlowFieldEffect {
         this.lastTime = timestamp
 
         if (this.timer > this.interval) {
-            this.angle += 0.1;
+            // this.angle += 0.1;
             this.#ctx.clearRect(0, 0, this.#width, this.#height)
-            this.#draw(this.#width / 2, this.#height / 2);
+
+            for (let y = 0; y < this.#height; y += this.cellSize) {
+                for (let x = 0; x < this.#width; x += this.cellSize) {
+                    this.#drawLine(x, y);
+                }
+            }
+
+
             this.timer = 0
         } else {
             this.timer += deltaTime
