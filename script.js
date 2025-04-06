99fde9186895484f0d2ac017d0975a772102159a
unknown <srijancodex@gmail.com>
2025-04-06 17:23:43 +0530

    javascript class and private class feature

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// 5:16
+// 5:14
 window.onload = function () {
     const canvas = this.document.getElementById('canvas1');
     const cxt = canvas.getContext('2d');
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const flowField = new FlowFieldEffect(cxt, canvas.width, canvas.height)
+}
+
+class FlowFieldEffect {
+    // private class field
+    #ctx;
+    #width;
+    #height;
+
+    constructor(cxt, width, height) {
+        this.#ctx = cxt;
+        this.#height = height;
+        this.#width = width;
+    }
 }
