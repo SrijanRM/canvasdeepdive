38e637c4d94c32a3547cb49ab8a3355409aa1dc2
unknown <srijancodex@gmail.com>
2025-04-06 17:18:07 +0530

    setting up html css js

---
+++ script.js
@@ -0,0 +1,8 @@
+// 5:16
+window.onload = function () {
+    const canvas = this.document.getElementById('canvas1');
+    const cxt = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
